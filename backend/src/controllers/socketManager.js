52d3b817bc02157cf6ffe09d1a27b744eaa5730e
Ayush tiwari <ayushtiwari92205@gmail.com>
2026-06-14 21:32:26 +0530

    socket setup

--- backend/src/controllers/socketManager.js
+++ backend/src/controllers/socketManager.js
@@ -2,10 +2,33 @@
 import { Server } from "socket.io";
 
 
+
+let connections={}
+let messages={}
+let timeOnline={}
+
+
 const connectToSocket =(server)=>{
     const io= new Server(server);
 
-    return io;
+   io.on("connection",(socket)=>{
+      socket.on("join-call",(path)=>{
+         // Handle join-call event
+         if(connections[path]===undefined){
+            connections[path]=[socket.id]
+         }connections[path].push(socket.id)
+      });
+
+      socket.on("signal",(toId,message)=>{
+        io.to(toId).emit("signal",socket.id,message);
+      })
+      socket.on("chat-message",(message)=>{
+        // Handle chat-message event
+      })
+      socket.on("disconnect",()=>{
+        // Handle disconnect event
+      })
+   });
 }
 
 export default connectToSocket;
